1b904901b6e5403ba070e20c533be23732ca9ec2
wei chen <weichen9083@gmail.com>
2019-09-20 01:32:01 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -21,7 +21,7 @@ end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	programmer_hash[:known_for][:alan_kay]
+
 	
 	programmer_hash = 
  		{
@@ -64,6 +64,7 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
+
 
 	programmer_hash = 
  		{
