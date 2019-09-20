88c8be9c91204b8290195c36c8a3d96a90193612
wei chen <weichen9083@gmail.com>
2019-09-20 02:04:28 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,9 +83,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash= [:yukihiro_matsumoto] = 
- { :known_for => "Ruby",
-  :languages => ["LISP", "C"]}
+programmer_hash= [:yukihiro_matsumoto] = { :known_for = "Ruby",
+  :languages = ["LISP", "C"]}
   
 end
 
@@ -107,7 +106,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-   programmer_hash[:alan_kay][:known_for] = "GUI"
+
      
 end
 
