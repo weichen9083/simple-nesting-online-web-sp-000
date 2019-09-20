8c659237c3fb5a37bfb6b20b23e6f68cce3f6596
wei chen <weichen9083@gmail.com>
2019-09-20 01:43:24 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,9 +83,9 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash[:yukihiro_matsumoto] = {
+programmer_hash = {:yukihiro_matsumoto =>{
   :known_for => "Ruby",
-  :languages => ["LISP", "C"]
+  :languages => ["LISP", "C"] }
 }
     
 end
